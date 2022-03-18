ac8fa463997d5b54274d21c744fbd97b18d432ed
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-03-18 19:51:43 +0300

    Added Task 1 to a separate project

---
+++ Bars_Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Program;
+static class Program
+{
+    private static void Main()
+    {
+        var sub = new Subscriber();
+        sub.SomeMethod();
+    }
+}
+
+public class Worker
+{
+    public event EventHandler<char> OnKeyPressed;
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Write("Write your symbol: ");
+            var x = Console.ReadKey();
+            if (x.Key == ConsoleKey.C) // 
+            {
+                Console.WriteLine(" Exit...");
+                break;
+            }
+            else
+            {
+                OnKeyPressed?.Invoke(this, x.KeyChar);
+            }
+        }
+    }
+}
+
+public class Subscriber
+{
+    public void SomeMethod()
+    {
+        var worker = new Worker();
+        worker.OnKeyPressed += PrintChar;
+        worker.Run();
+    }
+
+    public void PrintChar(object sender, char letter)
+    {
+        Console.WriteLine($" Symbol: {letter}");
+    }
+}
